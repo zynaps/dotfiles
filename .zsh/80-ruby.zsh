98003f42a44f38c233a61c5ea3bfbcd948a6f60f
Igor Vinokurov <zynaps@zynaps.ru>
2020-08-15 06:40:56 +0300

    support for brew installed ruby

--- .zsh/80-ruby.zsh
+++ .zsh/80-ruby.zsh
@@ -1,3 +1,7 @@
+if [ -d /usr/local/opt/ruby/ ]; then
+    export PATH="/usr/local/opt/ruby/bin:$PATH"
+fi
+
 if which ruby &> /dev/null; then
     export GEM_HOME=$(ruby -e "puts Gem.user_dir")
     export PATH="$GEM_HOME/bin":$PATH
